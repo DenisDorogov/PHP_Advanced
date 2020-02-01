fb989678953452ed3e22d42300abb45c71503d47
DenisDorogov <denisdorogov@yandex.ru>
2020-02-01 13:34:58 +0500

    Скорректировал пути.

--- views/catalog.php
+++ views/catalog.php
@@ -1,7 +1,7 @@
 <h2>Каталог</h2>
 <!--TODO Реализовать TWIG-->
 <?php foreach ($catalog as $key => $value) :?>
-    <a href="?&a=card&id=<?=$catalog[$key]['id']?>">
+    <a href="/product/card/?id=<?=$catalog[$key]['id']?>">
         <img src="<?=IMG_PATH_MIN . $catalog[$key]['img']?>" alt=""><br>
         <b><?=$catalog[$key]['name']?></b><br>
         <?=$catalog[$key]['description']?><br> Цена:
