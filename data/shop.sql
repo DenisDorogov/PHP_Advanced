59a21a6be67fd507ce558365ad1cf55abb42e69c
DenisDorogov <denisdorogov@yandex.ru>
2020-02-03 18:17:55 +0500

    Заменил дамп на созданный в phpMyAdmin. admin 123

--- data/shop.sql
+++ data/shop.sql
@@ -1,120 +1,153 @@
-CREATE DATABASE  IF NOT EXISTS `shop` /*!40100 DEFAULT CHARACTER SET utf8 */;
-USE `shop`;
--- MySQL dump 10.13  Distrib 8.0.16, for Win64 (x86_64)
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1    Database: shop
--- ------------------------------------------------------
--- Server version	5.6.43-log
+-- Хост: 127.0.0.1:3307
+-- Время создания: Фев 03 2020 г., 16:12
+-- Версия сервера: 5.6.43-log
+-- Версия PHP: 7.3.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
- SET NAMES utf8 ;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
--- Table structure for table `basket`
+-- База данных: `shop`
 --
 
-DROP TABLE IF EXISTS `basket`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `basket`
+--
+
 CREATE TABLE `basket` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `product_id` varchar(45) DEFAULT NULL,
+  `id` int(11) NOT NULL,
+  `product_id` varchar(11) DEFAULT NULL,
   `count` varchar(45) DEFAULT NULL,
   `session_id` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id_UNIQUE` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `price` double DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `order_id` varchar(11) DEFAULT NULL,
+  `is_in_order` tinyint(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `basket`
+-- Дамп данных таблицы `basket`
 --
 
-LOCK TABLES `basket` WRITE;
-/*!40000 ALTER TABLE `basket` DISABLE KEYS */;
-INSERT INTO `basket` VALUES (1,'2','1','lk8cp65p4bm31dahlvqndusbvkfa1jnk'),(2,'3','1','tegtthul8uap15pbtt4aakhu15hjfku7'),(7,'2',NULL,NULL),(16,'1',NULL,NULL),(17,'7',NULL,NULL),(18,'6',NULL,NULL),(19,'4',NULL,NULL);
-/*!40000 ALTER TABLE `basket` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `basket` (`id`, `product_id`, `count`, `session_id`, `price`, `user_id`, `order_id`, `is_in_order`) VALUES
+(1, '2', '1', 'lk8cp65p4bm31dahlvqndusbvkfa1jnk', NULL, NULL, NULL, NULL),
+(2, '3', '1', 'tegtthul8uap15pbtt4aakhu15hjfku7', NULL, NULL, NULL, NULL),
+(43, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(44, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `products`
+-- Структура таблицы `products`
 --
 
-DROP TABLE IF EXISTS `products`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
 CREATE TABLE `products` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `name` varchar(45) NOT NULL,
   `description` text,
   `price` int(7) DEFAULT NULL,
   `img` varchar(45) DEFAULT NULL,
-  `category` varchar(32) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id_UNIQUE` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `category` varchar(32) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `products`
+-- Дамп данных таблицы `products`
 --
 
-LOCK TABLES `products` WRITE;
-/*!40000 ALTER TABLE `products` DISABLE KEYS */;
-INSERT INTO `products` VALUES (1,'Яблоки','Красные',55,'apples.jpg','fruits'),(2,'Груши','Сладкие',60,'pears.jpg','fruits'),(3,'Бананы','Жёлтые',45,'bananas.jpg','fruits'),(4,'Хлеб','Кирпич',25,'bread.jpg','bakery'),(5,'Пирожные','Цветные',25,'cake_mini.jpg','bakery'),(6,'Помидоры','Турецкие',50,'tomatos.jpg','vegetables'),(7,'Огурцы','Пупырчатые',45,'cucumbers.jpg','vegetables');
-/*!40000 ALTER TABLE `products` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `products` (`id`, `name`, `description`, `price`, `img`, `category`) VALUES
+(1, 'Яблоки', 'Красные', 55, 'apples.jpg', 'fruits'),
+(2, 'Груши', 'Сладкие', 60, 'pears.jpg', 'fruits'),
+(3, 'Бананы', 'Жёлтые', 45, 'bananas.jpg', 'fruits'),
+(4, 'Хлеб', 'Кирпич', 25, 'bread.jpg', 'bakery'),
+(5, 'Пирожные', 'Цветные', 25, 'cake_mini.jpg', 'bakery'),
+(6, 'Помидоры', 'Турецкие', 50, 'tomatos.jpg', 'vegetables'),
+(7, 'Огурцы', 'Пупырчатые', 45, 'cucumbers.jpg', 'vegetables'),
+(13, 'Тестовый продукт', 'необычный', 600, NULL, NULL);
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Структура таблицы `users`
 --
 
-DROP TABLE IF EXISTS `users`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
 CREATE TABLE `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(32) NOT NULL,
+  `id` int(11) NOT NULL,
+  `login` varchar(32) NOT NULL,
   `pass` varchar(45) DEFAULT NULL,
-  `hash` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `name_UNIQUE` (`name`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `hash` varchar(45) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `users`
+-- Дамп данных таблицы `users`
 --
 
-LOCK TABLES `users` WRITE;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'Петя','123',NULL),(2,'Вася','456',NULL);
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `users` (`id`, `login`, `pass`, `hash`) VALUES
+(1, 'admin', '123', NULL),
+(2, 'john', '123', NULL),
+(3, 'alex', '123', NULL);
 
 --
--- Dumping events for database 'shop'
+-- Индексы сохранённых таблиц
 --
 
 --
--- Dumping routines for database 'shop'
+-- Индексы таблицы `basket`
 --
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+ALTER TABLE `basket`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id_UNIQUE` (`id`);
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+--
+-- Индексы таблицы `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id_UNIQUE` (`id`);
+
+--
+-- Индексы таблицы `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `name_UNIQUE` (`login`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `basket`
+--
+ALTER TABLE `basket`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
+
+--
+-- AUTO_INCREMENT для таблицы `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT для таблицы `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2020-01-31 17:07:37
